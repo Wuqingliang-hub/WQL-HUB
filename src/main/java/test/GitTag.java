0d08d8c6a2691326dea1ec3d8273b0d696d64530
jiang08ms <2472029659@qq.com>
2026-03-05 08:49:49 +0800

    这里写你的修改说明，比如：修复了XX功能，新增了XX文件

--- src/main/java/test/GitTag.java
+++ src/main/java/test/GitTag.java
@@ -2,7 +2,7 @@ package test;
 
 public class GitTag {
     public static void main(String[] args) {
-        System.out.println("Version2.0");
-        System.out.println("我是林泽轩，完成了修改");
+        System.out.println("Version3.0");
+        System.out.println("我是江敏深，完成了修改");
     }
 }
